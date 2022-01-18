c5f80d203d186a65fd148560d368ea4208d61356
TahirMansuri <tprocks786@gmail.com>
2022-01-18 18:46:20 +0530

    MODIFIED: Checking Max from 3 Float Object

--- src/com/bridgelabz/JavaGenericDemo.java
+++ src/com/bridgelabz/JavaGenericDemo.java
@@ -18,10 +18,28 @@ public class JavaGenericDemo {
         }
     }
 
+    //compareTo method for Checking Maximum from 3 Float Object
+    public static Float compareTo(Float a,Float b,Float c) {
+        if(a > b){
+            if(a > c) {
+                return a;
+            } else {
+                return c;
+            }
+        } else {
+            if(b > c) {
+                return b;
+            } else {
+                return c;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         //Welcome message for User
         System.out.println("Welcome to Java Core - Generics Test Maximum Developed by Tahir Mansuri.");
 
-        System.out.println("Maximum from 3 Integer Object : "+compareTo(90,120,20));
+        System.out.println("Maximum from 3 Integer Object : "+compareTo(90,120,190));
+        System.out.println("Maximum from 3 Float Object : "+compareTo(90.0F,120.0F,190.0F));
     }
 }
