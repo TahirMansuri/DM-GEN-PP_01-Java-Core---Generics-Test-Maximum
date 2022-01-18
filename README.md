f7020304f46409af3b193e99fa098771d408a214
TahirMansuri <tprocks786@gmail.com>
2022-01-18 18:37:35 +0530

    ADDED: Java Generic Problem

---
+++ README.md
@@ -0,0 +1,1 @@
+# Welcome to Java Core - Generics Test Maximum
---
+++ src/com/bridgelabz/JavaGenericDemo.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class JavaGenericDemo {
+    public static void main(String[] args) {
+        //Welcome message for User
+        System.out.println("Welcome to Java Core - Generics Test Maximum Developed by Tahir Mansuri.");
+    }
+}
